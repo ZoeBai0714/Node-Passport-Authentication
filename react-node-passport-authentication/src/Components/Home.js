19370d6deecb8e21e7cbbe1e0665b3baf89c05a5
github username <github email address>
2019-06-19 12:23:43 -0500

    imported css files

---
+++ react-node-passport-authentication/src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Home extends React.Component{
+   render(){
+       return(
+           <div className = "row mt-5">
+               Create an account or login
+               <div className ="col-md-6 m-auto"></div>
+               <div className ="card card-body text-center"></div>
+          </div>
+       )
+   }
+}
+
+export default Home;
