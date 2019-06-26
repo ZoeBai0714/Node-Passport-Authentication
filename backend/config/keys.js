4089c6613bdfd897ae335113b8c28b97a1f7af4c
github username <github email address>
2019-06-26 17:13:33 -0500

    changed Mongodb key to earlier node version

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,6 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://project:Udbp8lzPTlJ24z0T@test-0eu5k.mongodb.net/test?retryWrites=true&w=majority'
+    MongoURI: '//mongodb://project:Udbp8lzPTlJ24z0T@test-shard-00-00-0eu5k.mongodb.net:27017,test-shard-00-01-0eu5k.mongodb.net:27017,test-shard-00-02-0eu5k.mongodb.net:27017/test?ssl=true&replicaSet=Test-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
+
+
+//mongodb://project:Udbp8lzPTlJ24z0T@test-shard-00-00-0eu5k.mongodb.net:27017,test-shard-00-01-0eu5k.mongodb.net:27017,test-shard-00-02-0eu5k.mongodb.net:27017/test?ssl=true&replicaSet=Test-shard-0&authSource=admin&retryWrites=true&w=majority
